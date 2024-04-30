3dde5f1cd35b20d3f90f78f1d912db90cebd9878
p134c0d3 <70922864+p134c0d3@users.noreply.github.com>
2024-04-30 18:18:14 -0500

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :raw_material
+  
+  # associations
+  has_one :raw_material
 
   # validations
   validates :car_number, :requested_date, :received_date, :extraction_start_date, :emptied_date, :released_date, presence: true
 
-  # associations
-  has_one :raw_material
 end
