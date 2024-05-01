adf0cb2769eb0dbf3bcdc2bb507028be9ab28d7a
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-04-30 22:55:34 -0500

    added raw_materials controller with crud methods and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # RawMaterial routes
+  get "/raw_materials" => "raw_material#index" # List all raw materials
+  get "/raw_materials/:id" => "raw_material#show" # Show a specific raw material
+  post "/raw_materials" => "raw_material#add" # Add a new raw material
+  put "/raw_materials/:id" => "raw_material#update" # Update a raw material
+  delete "/raw_materials/:id" => "raw_material#delete" # Delete a raw material
+  get "/raw_materials/:id/orders" => "raw_material#raw_material_orders" # List all orders for a specific raw material
 end
