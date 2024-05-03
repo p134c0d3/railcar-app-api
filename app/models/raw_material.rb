f078d356090de5a4d588e16693465551576828a4
awhitm1 <41929321+awhitm1@users.noreply.github.com>
2024-05-02 19:12:20 -0500

    fix materials validations

--- app/models/raw_material.rb
+++ app/models/raw_material.rb
@@ -1,6 +1,6 @@
 class RawMaterial < ApplicationRecord
   has_many :orders 
 
-  validates :name, presence: true, uniqueness: true
-  validates :price, presence: true, numericality: { greater_than: 0, less_than: 1000000}
+  validates :material_name, presence: true, uniqueness: true
+  
 end
