09be39be3e4a598bcf9aa38e184ddb4587aed493
p134c0d3 <70922864+p134c0d3@users.noreply.github.com>
2024-05-23 15:35:02 -0500

    Update cors.rb
    - Changed CORS link to prod link for front end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #change to front end url
-    origins 'localhost:4200'
+    origins 'https://railcar-app.vercel.app'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
